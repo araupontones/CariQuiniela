4c15f9ffb10ebe0cc792d78504ae2ee796d213d5
araupontones <43657130+araupontones@users.noreply.github.com>
2022-10-23 21:30:34 +0200

    save data

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
+# A robot to predict the World Cup matches
+
+Experiment to predict World Cup matches using historical data of qualified teams.
+
+#Description of directories
+
+## R_
+Code
+
+* 1.Scrap_lookup_teams_urls.R : It scraps the list of qualified countries from (fbref)["https://fbref.com/en/comps/1/World-Cup-Stats"]. Each country has its own unique ID that is later used to extract their historical data. The output of this scripts is data/1.lookups/teams_urls.csv that is later used as the lookup table to interact across all the IDs in the system.
+
+* 2.Scrap_games_teams.R : It uses the output from 1.Scrap_*_urls.R and downloads all the matches for all the teams since 2012. The output of this code is data/2.scrapped/All_competitions.csv that is later used to create some of the indicators
+
+* 3.Create_indicators.R : It takes the output from 2.Scrap_*_teams.R, it cleans it and creates indicators at the year and team level. the output is stored in "data/3.clean"
+
+## data
+
+The relevant data for the analysis is stored in data/3.clean
+
+* "ind_all_matches.csv" : every result for all the qualified teams of Qatar 2022
+
+*  "ind_teams_year.csv" : indicators summarized by year and team
+
+
+
 
--- R_/3.Create_indicators.R
+++ R_/3.Create_indicators.R
@@ -6,29 +6,44 @@ library(stringr)
 library(janitor)
 
 indir <- "data/2.scrapped"
+#all_competitions.csv is created in R_/scrap_games_teams.R
 infile <- file.path(indir,"All_competitions.csv")
 
+#Exit paths ---------------------------------------------------------------------
+exdir <- "data/3.clean"
+exfile_matches <- file.path(exdir, "ind_all_matches.csv")
+exfile_year_team <-file.path(exdir, "ind_teams_year.csv")
+
+
+#read scrapped data ----------------------------------------------------------
 all_comps <- import(infile)
 
 
-#Define key variables for grouping  & drop duplicates---------------------------
+#Clean table of all matches & define year variable ---------------------------
 
-db_to_group <- all_comps %>%
-  #drop not played matches
+
+db_matches <- all_comps %>%
+  #drop not played matches (these are matches to be played in the future)
   filter(GF != "") %>%
-  #drop penalties in drawn matches
+  #drop penalties in drawn matches, penalties are reported within parenthesis.
   mutate(across(c(GF, GA) , function(x) as.numeric(str_replace(x, "\\([0-9]\\)",""))),
-         year = str_sub(Date,1,4)
+         year = str_sub(Date,1,4),
+         Opponent = str_remove(Opponent, "^[a-z][a-z] ")
          ) %>%
-  relocate(year) %>%
+  select(year, team, Opponent, Date,Time, Comp, Day, Venue, Result, GF, GA) %>%
+  #remove duplicates (there were some duplicated matches while scrapping)
   distinct()
+
+
+
          
 
-  db_to_group %>% janitor::tabyl(Sh)
+ 
 
 #Define indicators by year and country ---------------------------------------
 
-data_year <- db_to_group %>%
+data_year_team <- db_matches %>%
+  #create indicators by team and year
   group_by(team, year) %>%
   summarise(matches = n(),
             matches_win = sum(Result == "W"),
@@ -42,5 +57,13 @@ data_year <- db_to_group %>%
             Gdiff = GF - GA,
             .groups = 'drop'
             ) %>%
+  #norm indicators by number of matches played
   mutate(across(-c(team, year, starts_with("matches"), efectividad), ~ .x/matches, .names = "nrm_{.col}")) %>%
   relocate(year, team, starts_with("matches"), GF, GA, Gdiff)
+
+
+#===============================================================================
+#export data====================================================================
+
+export(db_matches, exfile_matches)
+export(data_year_team, exfile_year_team)
