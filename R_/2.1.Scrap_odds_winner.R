0b1198a10455217928fd1d819cc7276996ebc750
araupontones <43657130+araupontones@users.noreply.github.com>
2022-10-24 23:32:40 +0200

    scrap odds table

---
+++ R_/2.1.Scrap_odds_winner.R
@@ -0,0 +1,71 @@
+
+library(rvest)
+library(janitor)
+library(dplyr)
+
+exdir <- "data/2.scrapped"
+exfile <- file.path(exdir,"odds_winner.csv")
+
+
+#read website ----------------------------------------------------------------
+# from https://www.oddschecker.com/football/world-cup/winner
+# the site was locked so I created a manually downloaded the .html
+
+
+
+url <-"raw_htmls/winner.html"
+website <- read_html(url)
+
+#read table of odds -----------------------------------------------------------
+
+odds_table <- website %>%
+  rvest::html_elements("table") %>%
+  html_elements("tbody") %>%
+  html_table() %>%
+  .[[1]] %>%
+  janitor::remove_empty(which = "cols")
+
+
+
+
+#ways -------------------------------------------------------------
+#each bet was a way (1/2, 1/4. etc)
+# I download these separately so I can create the names of the variables later
+
+ways <- website %>%
+  rvest::html_elements("table") %>%
+  html_elements("thead") %>%
+  html_table() %>%
+  .[[1]] %>%
+  filter(X1 == "Each-way terms") %>%
+  #mutate(across(starts_with("X"), ~ ifelse(.x == "", NA, .x))) %>%
+  janitor::remove_empty(which = "cols")
+  
+#vector of ways
+ways_ <- as.character(ways[1,-1])
+
+
+
+
+
+#names of the bet companies ----------------------------------------
+names_houses <- website %>%
+  html_elements(".bk-logo-main-90") %>%
+  html_attr("title") 
+
+
+#keep only those that exist in the relevant table (i know this by the odds)
+house_ <- names_houses[1:length(ways_)]
+
+#create the names of the oods table ---------------------------------------
+
+#combine company with ways
+house_ways <- paste(house_, ways_, sep = "-")
+#add country to the names
+col_names <- c("country", house_ways)
+names(odds_table) <- col_names
+
+#================================================================================
+#export 
+
+rio::export(odds_table, exfile)
