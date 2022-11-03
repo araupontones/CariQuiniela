c37bf2618827b5c27d2fc55ddf724cce1d50c142
Andres Arau <araupontones@gmail.com>
2022-11-03 16:18:40 +0100

    Wallee: 03 Nov 2022 16:18:40

--- myterminal.sh
+++ myterminal.sh
@@ -3,5 +3,5 @@
 #git config --global user.email "araupontones@gmail.com"
 timestamp=$(date +%c)
 git add .
-git commit -m "Backup at: $timestamp"
+git commit -m "Wallee: $timestamp"
 git push origin main
