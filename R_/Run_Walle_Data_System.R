0a54f8db12b13ad70be8af17b8df64eac72ccbf4
araupontones <43657130+araupontones@users.noreply.github.com>
2022-11-08 14:07:05 +0100

    so walle reads from intermediate data from github

--- R_/Run_Walle_Data_System.R
+++ R_/Run_Walle_Data_System.R
@@ -33,6 +33,7 @@ dir_r <- file.path(get_proj_dir(), "R_")
 
 #source(file.path(dir_r,"fbref_matches/2.Scrap_games_2022_before_WC.R")) #exports to data/2.scrapped/fbref/matches_2022_before_WC.csv
 
+
 #2. Clean matches before the WC
 
 #source(file.path(dir_r,"fbref_matches/4.clean_matches_before_WC.R")) #exports to data/2.1intermediate/matches_before_WC.rds
@@ -49,13 +50,8 @@ source(file.path(dir_r,"request_WC_scores.R"), encoding = "UTF-8") #exports to d
 source(file.path(dir_r, "Append_before_and_WC.R"), encoding = "UTF-8")
 
 #Download oods to win the World Cup --------------------------------------------
-source(file.path(dir_r,"request_WC_odds_winners.R"), encoding = "UTF-8")
+source(file.path(dir_r,"request_WC_odds_winners.R"), encoding = "UTF-8") #saves ind_ods_einner.csv
 
-
-#Commit and push to repo -----------------------------------------------------
-
-#temp_terminal <- rstudioapi::terminalExecute("myterminal.sh")
-#rstudioapi::terminalKill(temp_terminal)
 
 
 
