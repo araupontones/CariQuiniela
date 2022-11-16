1726d6b75bb8b899ff3cb54cfd6dda96809f7223
Andres Arau <araupontones@gmail.com>
2022-11-16 16:54:46 +0100

    years_team for all teams

--- R_/Append_before_and_WC.R
+++ R_/Append_before_and_WC.R
@@ -37,94 +37,15 @@ all_matches <-  rbind(wc$scores, pre_wc$scores ) %>%
  
 
 
-
-
-
-
  
 #==============================================================================
 # Data of teams by year (indicators by Away, Home, Neutral and all matches)
 
 # Function to create indicators at the different venue -------------------------
-indicators_matches <- function(.data, prefix, venues = c("Away", "Home", "Neutral")){
-  
-  .data %>%
-    #keep only venue of interest
-    filter(Venue %in% venues) %>%
-    group_by(team, year) %>%
-    filter(!is.na(GF)) %>%
-    #normalize by FIFA index
-    mutate(fail_GF_index = (GF==0) * visitante_fifa_index,
-           fail_GA_index = (GA==0) * 1/visitante_fifa_index,
-           GF_index = GF * visitante_fifa_index,
-           GA_index = GA * 1/visitante_fifa_index
-           
-           ) %>%
-    #Puntos roy
-    mutate(puntos_roy = case_when(GF > 0 & GA == 0 ~ 5,
-                                  GF > 0 & Result == "W" ~ 4,
-                                  GF > 0 & (GA - GF ==1) & Result == "L" ~ 3,
-                                  GF > 0 & Result == "D" ~ 2,
-                                  GF > 0 & (GA - GF > 1) & Result == "L" ~ 1,
-                                  T ~ 0
-                                  ),
-           puntos_roy_index = puntos_roy * visitante_fifa_index
-           ) %>%
-    #summarise by year
-    summarise("matches" := n(),
-              "matches_win" := sum(Result == "W"),
-              "matches_lost" := sum(Result == "L"),
-              "matches_drawn" := sum(Result == "D"),
-              "efectividad" := matches_win/matches,
-              "fail_GF" := sum(GF == 0),
-              fail_GF_index = sum(fail_GF_index,na.rm = T),
-              "fail_GA" := sum(GA == 0),
-              fail_GA_index = sum(fail_GA_index, na.rm = T),
-              "GF" := sum(GF),
-              GF_index = sum(GF_index, na.rm = T),
-              "GA" := sum(GA),
-              GA_index = sum(GA_index, na.rm = T),
-              "Gdiff" := GF - GA,
-              puntos_roy = sum(puntos_roy),
-              puntos_roy_index = sum(puntos_roy_index),
-              .groups = 'drop'
-    ) %>%
-    #normalized by the number of matches played
-    mutate(across(-c(team, year, starts_with("matches"), efectividad), ~ .x/matches, .names = "nrm_{.col}")) %>%
-    relocate(year, team, starts_with("matches"), GF, GF_index, GA,GA_index, Gdiff) %>%
-    #rename variables based on the venue played
-    rename_at(vars(-c("team", "year")), function(x)paste(prefix,x, sep = "_"))
-  
-}
-
-
-
-# function to loop over all the venue types to create indicators --------------
-create_data_year <- function(.data, venues = c("Away", "Home", "Neutral")){
-  
-  #all games
-  all_ <- indicators_matches(.data, prefix = "all", venues )
-  
-  #games by venue
-  my_list <- lapply(venues, function(v){
-    
-    print(v)
-    my_data <- indicators_matches(.data, prefix = v, venues = v)
-    
-    
-  })
-  
-  #append data
-  appended_venues <- plyr::join_all(my_list, by = c("team", "year"))
-  appended <- left_join(all_, appended_venues, by = c("team", "year"))
-  
-}
 
 
 #===============================================================================
 # create data at the team and year level --------------------------------------
-
-
 
 data_year_team <- rbind(pre_wc$by_team, wc$by_team) %>%
   #get fifa quarters
@@ -141,15 +62,31 @@ data_year_team <- rbind(pre_wc$by_team, wc$by_team) %>%
                                           Opponent == "French Guiana" ~ 0.009130081,
                                           Opponent == "SÃ£o TomÃ©" ~ 0.008130081,
                                           Opponent == "San Marino" ~ 0.009130081,
-                                          T ~ visitante_fifa_index)) %>%
-  
-  create_data_year(.) 
+                                          T ~ visitante_fifa_index)) 
 
+  mundial <- create_data_year(data_year_team, 
+                               venues = c("Away", "Home", "Neutral"), 
+                               type = "WC", 
+                               wc_teams =look_up$team,
+                               goles_favor = GF,
+                               goles_contra = GA) 
+
+
+  no_qualified <- create_data_year(data_year_team, 
+                                   venues = c("Away", "Neutral"), 
+                                   type = "Others", 
+                                   wc_teams =look_up$team,
+                                   goles_favor = GA,
+                                   goles_contra = GF) 
+  
+  data_year_team_all <- plyr::rbind.fill(mundial,no_qualified)
+
+ 
 
 #===============================================================================
 #export data====================================================================
 
 
 export(all_matches, exfile_matches)
-export(data_year_team, exfile_year_team)
+export(data_year_team_all, exfile_year_team)
 message("Matches are cleaned!")
